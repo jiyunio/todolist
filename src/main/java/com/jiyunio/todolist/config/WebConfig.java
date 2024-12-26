b39e7c2444db609730a9710c3881a54b3afbe668
jiyunio <146628970+jiyunio@users.noreply.github.com>
2024-12-26 13:41:31 +0900

    Update WebConfig.java

--- src/main/java/com/jiyunio/todolist/config/WebConfig.java
+++ src/main/java/com/jiyunio/todolist/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/")
                 .allowedOrigins("http://localhost:" + port)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
