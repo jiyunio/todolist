cfcb5dd30eb2585d2bd22cf4e43ebb86d4fbb897
jiyunio <jiyun3269@naver.com>
2024-12-27 03:13:29 +0900

    [Style] errorcode code 변경

--- src/main/java/com/jiyunio/todolist/global/customError/ErrorCode.java
+++ src/main/java/com/jiyunio/todolist/global/customError/ErrorCode.java
@@ -5,25 +5,25 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
     // 400 Bad Request
-    NOT_SAME_CONFIRM_PASSWORD("001_NOT_SAME_CONFIRM_PASSWORD", "비밀번호 확인이 맞지 않습니다."),
-    NO_ANYMORE_CATEGORY("002_NO_ANYMORE_CATEGORY", "카테고리는 1개 이상이어야 합니다."),
+    NOT_SAME_CONFIRM_PASSWORD("001", "비밀번호 확인이 맞지 않습니다."),
+    NO_ANYMORE_CATEGORY("002", "카테고리는 1개 이상이어야 합니다."),
 
     // 401 AUTHORIZATION
-    NO_AUTHORIZATION("011_NO_AUTHORIZATION", "접근 권한이 없습니다."),
-    WRONG_USERID("013_WRONG_USERID", "아이디가 맞지 않습니다."),
-    WRONG_PASSWORD("012_WRONG_PASSWORD", "비밀번호가 맞지 않습니다."),
+    NO_AUTHORIZATION("011", "접근 권한이 없습니다."),
+    WRONG_USERID("012", "아이디가 맞지 않습니다."),
+    WRONG_PASSWORD("013", "비밀번호가 맞지 않습니다."),
 
 
     // 404 Not Found
-    NOT_EXIST_MEMBER("041_NOT_EXIST_MEMBER", "회원이 존재하지 않습니다."),
-    NOT_EXIST_TODO("042_NOT_EXIST_TODO", "TODO가 존재하지 않습니다."),
-    NOT_EXIST_TODOLIST("043_NOT_EXIST_TODOLIST", "TodoList가 존재하지 않습니다."),
-    NOT_EXIST_CATEGORY("044_NOT_EXIST_CATEGORY", "Category가 존재하지 않습니다."),
+    NOT_EXIST_MEMBER("041", "회원이 존재하지 않습니다."),
+    NOT_EXIST_TODO("042", "TODO가 존재하지 않습니다."),
+    NOT_EXIST_TODOLIST("043", "TodoList가 존재하지 않습니다."),
+    NOT_EXIST_CATEGORY("044", "Category가 존재하지 않습니다."),
 
     // 409 Conflict (중복된 값)
-    EXIST_USERID("091_EXIST_USERID", "이미 존재하는 아이디입니다."),
-    EXIST_EMAIL("092_EXIST_EMAIL", "이미 존재하는 이메일입니다."),
-    EXIST_TODOLIST("093_EXIST_TODOLIST", "이미 존재하는 todo list입니다.");
+    EXIST_USERID("091", "이미 존재하는 아이디입니다."),
+    EXIST_EMAIL("092", "이미 존재하는 이메일입니다."),
+    EXIST_TODOLIST("093", "이미 존재하는 todo list입니다.");
 
     private final String code;
     private final String message;
